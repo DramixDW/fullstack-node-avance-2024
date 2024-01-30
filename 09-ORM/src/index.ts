03069b29cd5f811dc0a6361f4d1bd872e8c5dd80
AmauryD <deflorenne.amaury@gmail.com>
2024-01-30 16:10:10 +0100

    operators

--- 09-ORM/src/index.ts
+++ 09-ORM/src/index.ts
@@ -1,4 +1,4 @@
-import { DataSource } from "typeorm";
+import { DataSource, In, LessThan, Raw } from "typeorm";
 import { Game, GameType } from "./models/games";
 
 async function init() {
@@ -9,7 +9,8 @@ async function init() {
         database: 'bot-theorie',
         password: 'bot-theorie',
         username: 'bot-theorie',
-        entities: ['src/models/*.ts']
+        entities: ['src/models/*.ts'],
+        logging: ['query']
     });
 
     await connection.initialize();
@@ -19,10 +20,24 @@ async function init() {
     await connection.synchronize();
 
     const games = await manager.find(Game, {
-
+        where: {
+            genre: Raw((columnAlias) => {
+                // typeorm n'a pas de support pour la recherche dans des SET, donc on fait à la main
+                return `FIND_IN_SET(:value, ${columnAlias} )`;
+            }, {
+                value: GameType.ACTION
+            })
+        }
     });
 
-    console.log(games);
+    // - 90 note
+    const badGames = await manager.find(Game, {
+        where: {
+           note: LessThan(90)
+        }
+    });
+
+    console.log(badGames);
     
 
     console.log("Connected");
