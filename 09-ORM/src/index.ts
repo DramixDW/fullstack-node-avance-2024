cbeadab8443c3cd6269a24329ba05eef0e14e62b
AmauryD <deflorenne.amaury@gmail.com>
2024-01-30 11:14:25 +0100

    init conn

--- 09-ORM/src/index.ts
+++ 09-ORM/src/index.ts
@@ -1,5 +1,16 @@
+import { DataSource } from "typeorm";
+
 async function init() {
-    
+    const connection = new DataSource({
+        type: 'mysql',
+        port: 3306,
+        host: 'localhost',
+        database: 'bot-theorie',
+        password: 'bot-theorie',
+        username: 'bot-theorie'
+    });
+
+    await connection.initialize();
 }
 
 // eslint-disable-next-line @typescript-eslint/no-floating-promises
