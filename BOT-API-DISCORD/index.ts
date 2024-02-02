7a1f99082d3fbfbac0355319924a8e6128aebd04
AmauryD <deflorenne.amaury@gmail.com>
2024-02-02 16:12:56 +0100

    programme minceur bot

--- BOT-API-DISCORD/index.ts
+++ BOT-API-DISCORD/index.ts
@@ -1,18 +1,6 @@
-import { Application, json } from "express";
-import express from "express";
-import { soundRouter } from "./Api/Routers/sounds.router";
-import { loggerMiddleware } from "./Api/Middlewares/logger.middleware";
-import { internalServerErrorHandler } from "./Api/Error-Handler/internal-server-error.handler";
-import { notFoundErrorHandler } from "./Api/Error-Handler/not-found-error.handler";
-import { NotFoundError } from "./Api/Errors/not-found.error";
-import mustacheExpress from "mustache-express";
-import { userRouter } from "./Api/Routers/users.router";
 import { DatabaseConnection } from "./Core/Database/connection";
 import { config } from "dotenv";
-import { authRouter } from "./Api/Routers/auth.router";
-import cookieParser from 'cookie-parser';
 import { seeder } from "./Core/Database/seeder";
-import { rateLimit } from 'express-rate-limit';
 import { initApi } from "./Api";
 
 // classes => PascalCase
