f68dc9910377adfb396835acfd0b004d3018a6c7
AmauryD <deflorenne.amaury@gmail.com>
2024-02-05 09:34:51 +0100

    ping command

--- BOT-API-DISCORD/Bot/index.ts
+++ BOT-API-DISCORD/Bot/index.ts
@@ -1,10 +1,26 @@
-import { Client, GatewayIntentBits } from 'discord.js';
+import { Client, GatewayIntentBits, Routes, SlashCommandBuilder } from 'discord.js';
+import { REST } from 'discord.js';
+
+async function registerCommands() {
+    const rest = new REST().setToken(process.env.BOT_TOKEN!);
+
+    await rest.put(Routes.applicationCommands(process.env.APPLICATION_ID!), {
+        body: [
+            new SlashCommandBuilder()
+            .setName('ping')
+            .setDescription('Ping me daddy')
+        ]
+    })
+}
+
 
 export async function initBot() {
     const client = new Client({ intents: [
         GatewayIntentBits.Guilds
     ] });
-    
+
+    await registerCommands();
+
     client.on('ready', () => {
         console.log("Le bot est connecté à Discord");
     });
