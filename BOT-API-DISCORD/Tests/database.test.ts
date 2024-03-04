fe22e885253d713c79cce12ac62aa95bf58f5172
AmauryD <deflorenne.amaury@gmail.com>
2024-03-04 15:08:10 +0100

    delete in database test

--- BOT-API-DISCORD/Tests/database.test.ts
+++ BOT-API-DISCORD/Tests/database.test.ts
@@ -1,8 +1,9 @@
 import { config } from "dotenv";
 import { DatabaseConnection } from "../Core/Database/connection";
-import { getUserById } from "../Core/Database/users";
+import { deleteUser, getUserById, insertUser } from "../Core/Database/users";
 import { Role, User } from "../Core/Models/users";
 import { EntityNotFoundError } from "../Api/Errors/entity-not-found.error";
+import { exec } from "child_process";
 
 async function seedForTesting() {
     const connection = DatabaseConnection.getConnection();
@@ -37,6 +38,39 @@ it('fetch users', async () => {
 
 it('throws an EntityNotFoundError when no User is found', async () => {
     await expect(() => getUserById('does-not-exists')).rejects.toThrow(EntityNotFoundError);
+});
+
+it('Should insert User into database', async () => {
+    const user = await insertUser({
+        username: 'LePetit',
+        lastName: 'greg',
+        firstName: 'banane',
+        password: '',
+        role: Role.USER
+    });
+
+    expect(user).toBeInstanceOf(User);
+    
+    const isInDatabase = await DatabaseConnection.getConnection().getRepository(User).exists({
+        where: {
+            username: 'LePetit'
+        }
+    })
+
+    expect(isInDatabase).toBe(true);
+});
+
+it('Should delete User in database', async () => {
+    const deletedUser = await deleteUser('timmy');
+
+    const isInDatabase = await DatabaseConnection.getConnection().getRepository(User).exists({
+        where: {
+            id: 'timmy'
+        }
+    });
+
+    expect(deletedUser).toBeInstanceOf(User);
+    expect(isInDatabase).toBe(false);
 });
 
 afterEach(async () => {
